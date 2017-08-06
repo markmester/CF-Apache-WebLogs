bd3255e4c4f774eabe4ea659e956b01c4d7bb8fb
admin <admin@quadrint.com>
2017-08-06 14:33:09 -0400

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Apache Log Pipeline
+#### AWS pipeline for for processing Apache httpd logs. Basic flow is as follows:
+Apache https logs -> S3 Bucket -> SQS trigger -> NiFi for parsing -> Kinesis/Kafka -> EMR/Redshift
+
+## Quick Deploy:
+1. Ensure AWS credentials can be found in: ~/.aws/credentials
+2. Deploy using the deploy.sh script: ```./deploy.sh create```
+
+
+## Notes:
+-   This pipeline completed up to the NiFi input stage. Still need to parse logs and send the EMR/Redshift.
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,24 +4,67 @@
 ACTION=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MYIP=`dig +short myip.opendns.com @resolver1.opendns.com`
-AWS_ACCOUNT_NUM=`aws ec2 describe-security-groups \
-                    --group-names 'Default' \
-                    --query 'SecurityGroups[0].OwnerId' \
-                    --output text`
+
+# Required if not using an AWS Instance Profile on httpd server
+AWS_ACCESS_KEY="$( cat ~/.aws/credentials | grep aws_access_key | awk '{print $3}' )"
+AWS_SECRET_ACCESS_KEY="$( cat ~/.aws/credentials | grep aws_secret_access_key | awk '{print $3}' )"
+
+function wait_completed {
+    STATUS="$( aws cloudformation describe-stacks --stack-name sqs | grep StackStatus )"
+
+    while [[ ${STATUS} != *"CREATE_COMPLETE"* ]]; do
+        echo "waiting for stack ${1} to complete..."
+        sleep 5
+    done
+}
 
 function create {
     echo ">>> Deploying stacks..."
 
-    aws cloudformation create-stack  --stack-name redshift --template-body file://deploy/cf-redshift.yml \
-        --parameters ParameterKey=KeyName,ParameterValue=gen_key_pair ParameterKey=MasterUserPassword,ParameterValue=MyPassword123 \
-        ParameterKey=VPCId,ParameterValue=vpc-1491216d ParameterKey=SubnetID,ParameterValue=subnet-0c219220 \
-        ParameterKey=SubnetCIDR,ParameterValue=172.31.64.0/24
+    aws cloudformation create-stack --stack-name httpd --template-body file://deploy/cf-apache-httpd.yml \
+        --parameters ParameterKey=KeyName,ParameterValue=gen_key_pair ParameterKey=DBRootPassword,ParameterValue=MyPassword123 \
+        ParameterKey=DBPassword,ParameterValue=MyPassword123 ParameterKey=SSHLocation,ParameterValue=${MYIP}/32 \
+        --capabilities CAPABILITY_IAM
+
+    wait_completed httpd
+
+    aws cloudformation create-stack --stack-name nifi --template-body file://deploy/cf-nifi.yml \
+        --parameters ParameterKey=KeyName,ParameterValue=gen_key_pair ParameterKey=RemoteLocation,ParameterValue=${MYIP}/32 \
+        --capabilities CAPABILITY_IAM
+
+    wait_completed nifi
+
+    aws cloudformation create-stack --stack-name sqs --template-body file://deploy/cf-sqs.yml \
+        --parameters ParameterKey=BucketName,ParameterValue=httpd-logs
+
+    wait_completed sqs
+
+    # get sqs queue arn
+    SQSQueueArn="$( aws sqs list-queues --queue-name-prefix SQSQueue | \
+        grep https://queue.amazonaws.com | \
+        awk '{$1=$1};1' | \
+        xargs aws sqs get-queue-attributes --attribute-names All --queue-url | \
+        grep QueueArn | \
+        awk '{print $2}' | \
+        sed 's/\"//g' )"
+
+    # get logs bucket name
+    LogsBucket="$( aws s3api list-buckets | grep httpd-logs | awk '{print $2}' | sed 's/\"//g' )"
+
+    # update notification config with sqs arn
+    sed -i "s;\"QueueArn\":.*;\"QueueArn\": \"${SQSQueueArn}\",;" deploy/config/log-bucket-notification-config.json
+
+    # create sqs queue notification
+    aws s3api put-bucket-notification-configuration --bucket ${LogsBucket}  \
+        --notification-configuration file://./deploy/config/log-bucket-notification-config.json
 
 }
 
 function delete {
     echo ">>> Deleting stacks..."
-    aws cloudformation delete-stack --stack-name redshift
+    aws cloudformation delete-stack --stack-name httpd
+    aws cloudformation delete-stack --stack-name nifi
+    aws cloudformation delete-stack --stack-name sqs
 
 }
 
